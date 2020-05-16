d6960895943db6a2cfc8e770562af8368cac26a3
Ruslan Bulyna <rus.bulyna@gmail.com>
2020-05-16 23:22:05 +0300

    update

--- src/javascript/fight.js
+++ src/javascript/fight.js
@@ -1,15 +1,40 @@
 export function fight(firstFighter, secondFighter) {
   var c = 0;
-  do {
+  var winner;
+  while(firstFighter.health > 0 || secondFighter.health > 0) {
     if (c == 0) {
       secondFighter.health -= getDamage(firstFighter,secondFighter);
       c = 1;
     } else {
       firstFighter.health -= getDamage(secondFighter,firstFighter);
       c = 0;
+
     }
-  } while(firstFighter.health <= 0 || secondFighter.health <= 0);
-  return firstFighter;
+
+    if(firstFighter.health <= 0) {
+      winner = secondFighter;
+      break;
+    }
+    else if (secondFighter.health <= 0) {
+      winner = firstFighter;
+      break;
+    }
+
+
+    console.log(firstFighter.health + ' _____ ' + secondFighter.health);
+  }
+  if(firstFighter.attack > secondFighter.attack){
+    if(firstFighter.defense > secondFighter.defense){
+      winner = firstFighter;
+    }
+  }
+  else if (secondFighter.attack >= firstFighter.attack){
+    if(secondFighter.defense > firstFighter.defense) {
+      winner = secondFighter;
+    }
+  }
+  
+  return winner;
 }
 
 export function getDamage(attacker, enemy) {
@@ -21,13 +46,14 @@ export function getDamage(attacker, enemy) {
 
 export function getHitPower(fighter) {
   const criticalHitChance = Math.floor(Math.random() * 2);
-  const power = fighter.attack * criticalHitChance;
+  const attack = fighter.attack;
+  const power = criticalHitChance * attack;
   return power;
 }
 
 export function getBlockPower(fighter) {
   const dodgeChance = Math.floor(Math.random() * 2);
-
-  const power = fighter.defense * dodgeChance;
+  const defense = fighter.defense;
+  const power = dodgeChance * defense;
   return power;
 }
